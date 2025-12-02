054ff8a2b252977237834508e87194554568334f
nafis589 <princenafistoure@gmail.com>
2025-12-02 21:24:42 +0100

    good

--- backend-image-cleaner/routes/imageRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { removeBgUrl } from '../controllers/imageController';
-import { checkApiKey } from '../middleware/auth';
-
-const router = Router();
-
-router.post('/remove-bg-url', checkApiKey, removeBgUrl);
-
-export default router;
